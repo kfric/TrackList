40d6aa1ee20efdfd7f270aa29003291eebea6813
Karl Frick <karl.frick5@gmail.com>
2021-07-26 19:06:32 -0400

    Adds package Tailwindcss.

--- ClientApp/src/App.jsx
+++ ClientApp/src/App.jsx
@@ -2,7 +2,5 @@ import React from 'react'
 import './custom.scss'
 
 export function App() {
-  return (
-    <div className="d-flex justify-content-center display-2">Hello, World!</div>
-  )
+  return <div className="text-center">Hello, World!</div>
 }
